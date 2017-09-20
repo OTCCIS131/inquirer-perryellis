a4036ca59d7e016161903eb09a2d969a7ac20a0b
Unknown <pe0877888@otc.edu>
2017-09-20 12:25:35 -0500

    answeres returned

--- Index.js
+++ Index.js
@@ -1,19 +1,25 @@
 const inquirer = require("Inquirer")
 const chalk = require("Chalk")
 
-let question = [
-   
-    {
-        name: 'size',
-        message: 'What size of pizza do you want?',
-    },
 
+console.log('Welcome to Mightys Pizza')
+
+let question = [{
+        name: 'size',
+        message: 'What size of pizza do you want?',   
+    },
     {
-        name: 'Toppings'
-    }
+        name: 'toppings',
+        message: "What toppings would you like?",  
+    },
+    {
+        name: 'cut',
+        message: 'How would you like your pizza cut?',
+    },
 ]
 
-inquirer.prompt(question).then(function (answers){
-    Console.log(answers)
-}
 
+inquirer.prompt(question).then(function ({size, toppings, cut}){
+    //Console.log(answers)
+    console.log( ("\n") + (chalk.green('Your Pizza Order: ')) + ("\n") + (chalk.yellow(' Pizza Size: ' + size)) + ("\n") + (chalk.blue(' Topping you orderd: ' + toppings)) + ("\n") + (chalk.red(' you wanted your pizza cut: ' + cut)))
+});
