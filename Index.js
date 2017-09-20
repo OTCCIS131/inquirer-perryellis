d587b68b6bdb228e2199b42c51ae5e0f5fe7b26b
Unknown <pe0877888@otc.edu>
2017-09-20 18:17:50 -0500

    working on the restart

--- Index.js
+++ Index.js
@@ -1,5 +1,8 @@
 const inquirer = require("Inquirer")
 const chalk = require("Chalk")
+const confirm = require('inquirer-confirm');
+
+
 
 
 console.log('Welcome to Mightys Pizza')
@@ -15,11 +18,20 @@ let question = [{
     {
         name: 'cut',
         message: 'How would you like your pizza cut?',
-    },
+    },  
 ]
-
 
 inquirer.prompt(question).then(function ({size, toppings, cut}){
     //Console.log(answers)
-    console.log( ("\n") + (chalk.green('Your Pizza Order: ')) + ("\n") + (chalk.yellow(' Pizza Size: ' + size)) + ("\n") + (chalk.blue(' Topping you orderd: ' + toppings)) + ("\n") + (chalk.red(' you wanted your pizza cut: ' + cut)))
+    console.log( ("\n") + (chalk.green('Your Pizza Order: ')) + ("\n") + (chalk.yellow(' Pizza Size: ' + size)) + ("\n") + (chalk.blue(' Toppings orderd: ' + toppings)) + ("\n") + (chalk.red(' Pizza cut: ' + cut)))
+   
+    confirm('Is your order correct? ')
+    .then(function confirmed(){
+        console.log('Great your order is being processed');
+    }, function cancelled(){
+        inquirer.prompt(question);
+        })
+        
 });
+
+
