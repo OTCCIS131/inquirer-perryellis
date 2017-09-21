6e198c852f674f38cb0ed66701c42b83fc38dc9e
Unknown <pe0877888@otc.edu>
2017-09-20 19:05:18 -0500

    added the figlet

--- Index.js
+++ Index.js
@@ -1,8 +1,13 @@
 const inquirer = require("Inquirer")
 const chalk = require("Chalk")
 const confirm = require('inquirer-confirm');
+const figlet = require('figlet')
 
-console.log('Welcome to Mightys Pizza')
+console.log(figlet.textSync('Mightys PIZZA', {
+    font: 'Dancing Font',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+}));
 
 let question = [{
         name: 'size',
@@ -28,12 +33,13 @@ inquirer.prompt(question).then(function ({size, toppings, cut}){
     //Console.log(answers)
     console.log( ("\n") + (chalk.green('Your Pizza Order: ')) + ("\n") + (chalk.yellow(' Pizza Size: ' + size)) + ("\n") + (chalk.blue(' Toppings orderd: ' + toppings)) + ("\n") + (chalk.red(' Pizza cut: ' + cut)))
    
+
     confirm('Is your order correct? ')
-    .then(function confirmed(){
-        console.log('Great your order is being processed');
-    }, function cancelled(){
-        inquirer.prompt(question).then(confirm);
-        })
+   .then(function confirmed(){
+       console.log('Great your order is being processed');
+   }, function cancelled(){
+       inquirer.prompt(question).then(confirm);
+       })
 
 });
 
